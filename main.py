37503520b033305d3916c33f9776ff116e8b54a7
realdragonhead <offend1ng02007@gmail.com>
2020-11-10 15:29:50 +0900

    Add main function make loop to insert keyword to cralwer function

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import subprocess
+import sys
 import csv
 import shlex
 import io
 
+keyword_file = sys.argv[1]
+i = 1
 
+f = io.open(keyword_file, 'r', encoding='utf-8')
+lines = f.readlines()
 
-f = io.open(
+for line in lines:
+	print(line)
+	
+	text = lines[i]
+	text = str(text)
+	text.replace(',', '')
+	text.replace(' ', '')
+	
+	print('searching ', text, ' in Instagram')
+	subprocess.call(['python3', 'crawler_routine.py', text])
+	
+f.close()
